913aafd51bd1a5accd042043a0e1523b3c3e5905
Robert Wlodarczyk <robert@simplicityguy.com>
2015-01-17 12:26:31 -0800

    Cleaning up Jawbone usage a tad.

--- force/login.py
+++ force/login.py
@@ -1,47 +1,56 @@
 from force import app
+
 from flask import redirect, request, make_response
 from redis import Redis
+
 import requests
 from jawbone.jawbone import Jawbone
 
-client_id = "KOls4_kxR_Q"
-client_secret = "d84b648530a3aa9029322c37fa77c75ce64664a4"
-redirect_uri = "http://192.168.59.103/login"
+site_root = "http://192.168.59.103"
+
+jawbone_client_id = "KOls4_kxR_Q"
+jawbone_client_secret = "d84b648530a3aa9029322c37fa77c75ce64664a4"
+jawbone_redirect_uri = "{0}/authorized/jawbone".format(site_root)
 scope = "basic_read,extended_read,location_read"
-token = None
 
 
 @app.route("/")
-def root():
-    jb = Jawbone(client_id, client_secret, redirect_uri, scope)
+def index():
+    return make_response("You've hit 4ce!", 200)
+
+
+@app.route("/jawbone")
+def root_jb():
+    jb = Jawbone(jawbone_client_id,
+                 jawbone_client_secret,
+                 jawbone_redirect_uri,
+                 scope)
     auth = jb.auth()
     return redirect(auth)
 
 
-@app.route("/login")
-def login():
+@app.route("/authorized/jawbone")
+def login_jawbone():
     code = request.args.get("code")
     if not code:
-        return make_response("Not authenticated!", 501)
+        return redirect(site_root)
 
-    jb = Jawbone(client_id, client_secret, redirect_uri, scope)
+    jb = Jawbone(jawbone_client_id,
+                 jawbone_client_secret,
+                 jawbone_redirect_uri,
+                 scope)
+
     token = jb.access_token(code)
     access_token = token["access_token"]
-#    refresh_token = token["refresh_token"]
 
-    endpoint = "/nudge/api/v.1.1/users/@me"
+    headers = {"Accept": "application/json",
+               "Authorization": "Bearer {0}".format(access_token)}
 
-    headers = {
-            'Accept': 'application/json',
-            'Authorization': 'Bearer {0}'.format(access_token)
-        }
-
-    response = requests.get("https://jawbone.com/nudge/api/v.1.1/users/@me", headers=headers)
-
-    print
-    print response.json()
+    response = requests.get("https://jawbone.com/nudge/api/v.1.1/users/@me",
+                            headers=headers)
 
     redis = Redis(host="redis_1", port=6379)
-    redis.incr("hits")
+    redis.incr("jawbone_hits")
 
-    return make_response("Success! ({0})".format(redis.get("hits")), 200)
+    return make_response("Success! ({0})\n{1}".format(
+        redis.get("jawbone_hits"), response.json()), 200)
